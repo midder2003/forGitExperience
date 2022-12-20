df5749bb26bfbff90012cc8dd64055191c5b19f5
Abdula Mukhumayev <abdula.mukhumayev@bk.ru>
2022-12-20 18:02:38 +0300

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by User on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
