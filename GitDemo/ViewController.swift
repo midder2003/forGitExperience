68223b7dd84e45d17be7536c0992159cc4ec14a6
Abdula Mukhumayev <97827277+midder2003@users.noreply.github.com>
2022-12-24 19:17:41 +0300

    Update ViewController with one additional print

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  GitDemo
-//
-//  Created by User on 20.12.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello")
+        print("Hello Hello")
     }
-
-
 }
 
